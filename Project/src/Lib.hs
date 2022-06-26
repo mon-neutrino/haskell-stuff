9c2345051376092ecc276b736c052ece21dd6a8f
mon-neutrino <monica.sing.2023@gmail.com>
2022-06-26 17:22:21 +0700

    list and list comprehension stuffs

--- Project/src/Lib.hs
+++ Project/src/Lib.hs
@@ -6,3 +6,16 @@ someFunc :: IO ()
 someFunc = putStrLn "rawr"
 
 doubleMoi x y = 3*x+4*y
+
+compFunc x y z a = doubleMoi x y + doubleMoi z a
+
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1 
+
+lostNumbers = [1,3,6,67,69]
+wack = ["omg","slay","wow"]
+yass' = ["y","a","s","s"]
+yass = ['y','a','s','s']
+wo = ['w','o']
+ot = ['o','t']
